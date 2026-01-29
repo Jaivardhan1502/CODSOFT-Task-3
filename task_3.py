dfccd67eabf52e80635b6f4ef9deeddefe94d13e
Poojala Jaivardhan <jaivardhan1502@gmail.com>
2026-01-29 09:57:08 +0530

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+print("---PASSWORD GENERATOR---")
+
+length = int(input("Enter desired length of password: "))
+characters= string.ascii_letters + string.digits + string.punctuation
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password: ",password)
